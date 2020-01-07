8698e06382cf5027577aaf6a1f1fb09c5d698d4b
Rhett <reticentcool@protonmail.com>
2020-01-07 00:51:40 -0500

    Revised everything

--- flpack.nim
+++ flpack.nim
@@ -3,76 +3,67 @@ import os
 import endians
 import strformat
 
+
 type
-    ChunkHeader = object
-        nextChunk: uint32
-        fileAmount: uint32
-
-    AssetHeader = object
-        nameLength: uint32
-        assetName: string
-        assetoffset: uint32
+    AssetEntry = object
+        name: string
+        dataOffset: uint32
         dataLength: uint32
-        checksum: uint32
+        dataHash: uint32
 
-    FlPackChunk = object
-        header: ChunkHeader
-        assets: seq[AssetHeader]
-    
     FlPack = object
         path: string
         name: string
-        chunks: seq[FlPackChunk]
+        assets: seq[ref AssetEntry]
 
+
+# Helper Functions
+proc newAssetEntry(): ref AssetEntry =
+    new(result)
 
 proc readSwappedUint32(inStream: FileStream): uint32 =
     var a = inStream.readUint32
     swapEndian32(result.addr, a.addr)
 
-proc loadFlPack(path: string): FlPack =
-    echo "Loading \"", path, "\"..."
+
+# Main Functions
+proc loadFlPack(path: string): ref FlPack =
+    new(result)
+    echo &"Loading '{path}'..."
 
     result.path = path
-    result.name = result.path.splitFile.name
+    result.name = splitFile(result.path).name
 
-    var inStream = newFileStream(result.path, fmRead)
-    if not inStream.isNil:
-        try:
-            # Loop until no more next chunks
-            var chunkCount = 0
-            while true:
-                var chunk: FlPackChunk
+    let inStream = newFileStream(result.path, fmRead)
+    if isNil(inStream): raise newException(IOError, &"'{path.absolutePath()}' doesn't exist")
+    defer: inStream.close()
 
-                echo &"\n### CHUNK {chunkCount}"
-                chunk.header.nextChunk = inStream.readSwappedUint32
-                chunk.header.fileAmount = inStream.readSwappedUint32
+    var chunkCount = 0
+    while true: # Loop until we run out of chunks
+        echo &"\n### CHUNK {chunkCount}"
+        let nextChunk = inStream.readSwappedUint32()
+        let fileAmount = inStream.readSwappedUint32()
 
-                echo &"# Next Chunk: {chunk.header.nextChunk:#010x}"
-                echo &"# Assets: {chunk.header.fileAmount}"
+        echo &"# Next Chunk: {nextChunk:#010x}"
+        echo &"# Assets: {fileAmount}"
 
-                # Loop through assets
-                for i in 0..<chunk.header.fileAmount:
-                    var asset: AssetHeader
+        for _ in 0..<fileAmount:
+            let asset = newAssetEntry()
 
-                    asset.nameLength = inStream.readSwappedUint32
-                    asset.assetName = inStream.readStr(asset.nameLength.int)
-                    asset.assetoffset = inStream.readSwappedUint32
-                    asset.dataLength = inStream.readSwappedUint32
-                    asset.checksum = inStream.readSwappedUint32
+            let nameLength = inStream.readSwappedUint32()
+            asset.name = inStream.readStr(int(nameLength))
+            # echo &"'{asset.name}'"
 
-                    echo asset.assetName
-                    chunk.assets.add(asset)
-                    
-                result.chunks.add(chunk)
+            asset.dataOffset = inStream.readSwappedUint32()
+            asset.dataLength = inStream.readSwappedUint32()
+            asset.dataHash = inStream.readSwappedUint32()
 
-                if chunk.header.nextChunk == 0:
-                    break
-                chunkCount.inc
-                inStream.setPosition(chunk.header.nextChunk.int)
-
-        finally:
-            inStream.close
+            result.assets.add(asset)
+        
+        if nextChunk == 0: break
+        inc(chunkCount)
+        inStream.setPosition(int(nextChunk))
 
 
 when isMainModule:
-    discard loadFlPack(r"Assets_000.pack")
+    discard loadFlPack(r"sample.pack")
